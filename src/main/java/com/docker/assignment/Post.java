370194485f0167766cabb3fd16bea354d1a0c5b8
lyndonyang <lyndonyang@yeah.net>
2020-07-28 10:39:36 +0800

    Update GenerationType for entity primary key.

--- src/main/java/com/docker/assignment/Post.java
+++ src/main/java/com/docker/assignment/Post.java
@@ -14,7 +14,7 @@ import javax.persistence.Table;
 public class Post {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="POST_ID")
 	Integer postId;
 	
